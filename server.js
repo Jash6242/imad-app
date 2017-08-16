31fc549d933e8c8ff808f52b66d6c29d4345ec5d
Jash6242 <30744976+Jash6242@users.noreply.github.com>
2017-08-16 12:09:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-new',function(req,res) {
-   res.sendFile(path.join(__dirname, 'ui', 'my new page.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'mynewpage.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
